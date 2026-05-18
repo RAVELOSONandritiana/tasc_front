a0db14e94a2f4efefcb03090652ccf7d2e87f975
michel HGB <hgbmichel@gmail.com>
2026-05-18 19:53:21 +0300

    add tooltip navigation

--- src/routes/app/anne-scolaire/[id]/+layout.svelte
+++ src/routes/app/anne-scolaire/[id]/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import { Portal, Tooltip } from '@skeletonlabs/skeleton-svelte';
 	import Link from '$lib/components/user/Link.svelte';
 
 	let { children } = $props();
@@ -7,10 +8,10 @@
 	const id = $page.params.id;
 
 	const links = [
-		{ path: `/app/anne-scolaire/${id}`, label: 'Surveillant' },
-		{ path: `/app/anne-scolaire/${id}/prof`, label: 'Professeur' },
-		{ path: `/app/anne-scolaire/${id}/classe`, label: 'Classe' },
-		{ path: `/app/anne-scolaire/${id}/parametre`, label: 'Paramètre' }
+		{ path: `/app/anne-scolaire/${id}`, label: 'Surveillant' ,comment: 'faire regner la loi'},
+		{ path: `/app/anne-scolaire/${id}/prof`, label: 'Professeur' ,comment: 'espace professeur'},
+		{ path: `/app/anne-scolaire/${id}/classe`, label: 'Classe' ,comment: 'gerer les classes'},
+		{ path: `/app/anne-scolaire/${id}/parametre`, label: 'Paramètre' ,comment: 'modifier les parametres selon votre besoin'}
 	];
 </script>
 
@@ -18,7 +19,22 @@
 	<nav class="py-3">
 		<ul class="mx-4 flex items-center gap-2">
 			{#each links as link (link.label)}
-				<Link label={link.label} href={link.path} />
+
+			<Tooltip>
+				<Tooltip.Trigger>
+					<Link label={link.label} href={link.path} />
+				</Tooltip.Trigger>
+				<Portal>
+					<Tooltip.Positioner>
+						<Tooltip.Content class="card bg-surface-100-900 p-2 shadow-xl">
+							<span>{link.comment}</span>
+							<Tooltip.Arrow class="[--arrow-size:--spacing(2)] [--arrow-background:var(--color-surface-100-900)]">
+								<Tooltip.ArrowTip />
+							</Tooltip.Arrow>
+						</Tooltip.Content>
+					</Tooltip.Positioner>
+				</Portal>
+			</Tooltip>
 			{/each}
 		</ul>
 	</nav>
