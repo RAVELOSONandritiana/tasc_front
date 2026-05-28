4b4a55f90756a3961d9e67f411cd49d829b53d3c
michel HGB <hgbmichel@gmail.com>
2026-05-28 21:18:09 +0300

    add confirm password

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,44 +9,35 @@
 	import Input from '$lib/components/ui/input/input.svelte';
 	import Label from '$lib/components/ui/label/label.svelte';
 
-    function gotoApp(event: Event){
-        event.preventDefault();
-        goto('/app');
-    }
+	function gotoApp(event: Event) {
+		event.preventDefault();
+		goto('/app');
+	}
 </script>
 
 <div class="flex min-h-screen items-center justify-center px-4">
 	<form class="w-full max-w-sm md:max-w-lg" onsubmit={gotoApp}>
 		<Card class="w-full">
 			<CardHeader>
-				<CardTitle class="mx-auto font-bold my-3">Connexion à Tasc</CardTitle>
-				<CardDescription>
-					Veuillez entrer vos informations de compte
-				</CardDescription>
+				<CardTitle class="mx-auto my-3 font-bold">Connexion à Tasc</CardTitle>
+				<CardDescription>Veuillez entrer vos informations de compte</CardDescription>
 			</CardHeader>
 
 			<CardContent class="space-y-4">
 				<div class="space-y-2">
 					<Label for="email">Email</Label>
-					<Input
-						id="email"
-						type="email"
-						placeholder="exemple@email.com"
-						required
-					/>
+					<Input id="email" type="email" placeholder="exemple@email.com" required />
 				</div>
 
 				<div class="space-y-2">
 					<Label for="password">Mot de passe</Label>
-					<Input
-						id="password"
-						type="password"
-						placeholder="********"
-						required
-					/>
+					<Input id="password" type="password" placeholder="********" required />
 				</div>
-				<a href="/signup" class="my-2 text-right">Create account</a>
-                <Button class="w-full" type="submit">Se connecter</Button>
+
+				<div class="relative h-10 py-2">
+					<a href="/signup" class="absolute right-0">Create account</a>
+				</div>
+				<Button class="w-full" type="submit">Se connecter</Button>
 			</CardContent>
 		</Card>
 	</form>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -9,44 +9,40 @@
 	import Input from '$lib/components/ui/input/input.svelte';
 	import Label from '$lib/components/ui/label/label.svelte';
 
-    function gotoSignin(event: Event){
-        event.preventDefault();
-        goto('/');
-    }
+	function gotoSignin(event: Event) {
+		event.preventDefault();
+		goto('/');
+	}
 </script>
 
 <div class="flex min-h-screen items-center justify-center px-4">
 	<form class="w-full max-w-sm md:max-w-lg" onsubmit={gotoSignin}>
 		<Card class="w-full">
 			<CardHeader>
-				<CardTitle class="mx-auto font-bold my-3">Cree un compte sur Tasc</CardTitle>
-				<CardDescription>
-					Veuillez entrer vos informations de compte
-				</CardDescription>
+				<CardTitle class="mx-auto my-3 font-bold">Cree un compte sur Tasc</CardTitle>
+				<CardDescription>Veuillez entrer vos informations de compte</CardDescription>
 			</CardHeader>
 
 			<CardContent class="space-y-4">
 				<div class="space-y-2">
 					<Label for="email">Email</Label>
-					<Input
-						id="email"
-						type="email"
-						placeholder="exemple@email.com"
-						required
-					/>
+					<Input id="email" type="email" placeholder="exemple@email.com" required />
 				</div>
 
 				<div class="space-y-2">
 					<Label for="password">Mot de passe</Label>
-					<Input
-						id="password"
-						type="password"
-						placeholder="********"
-						required
-					/>
+					<Input id="password" type="password" placeholder="********" required />
 				</div>
-				<a href="/signup" class="my-2 text-right">Aller vers connexion</a>
-                <Button class="w-full" type="submit">Creer compte</Button>
+
+				<div class="space-y-2">
+					<Label for="password">Confirmer mot de passe</Label>
+					<Input id="password" type="password" placeholder="********" required />
+				</div>
+
+				<div class="relative h-10 py-2">
+					<a href="/" class="absolute right-0">Aller vers connexion</a>
+				</div>
+				<Button class="w-full" type="submit">Creer compte</Button>
 			</CardContent>
 		</Card>
 	</form>
