7fe4095cf99ea359558412f78ca78fc3dde3c9d3
hashterx <hgbmichel@gmail.com>
2026-05-31 15:58:37 +0300

    form to add surveillant

--- src/routes/(app)/personne/+page.svelte
+++ src/routes/(app)/personne/+page.svelte
@@ -10,44 +10,5 @@
 	<div class="flex justify-between sticky top-0 z-50">
 		<Input type="search" placeholder="Rechercher une personne" class="max-w-md" />
 		<Button onclick={() => goto('personne/new')}>Creer</Button>
-		<!-- <Dialog.Root>
-			<form>
-				<Dialog.Trigger type="button" class={buttonVariants({ variant: 'outline' })}>
-					Creer
-				</Dialog.Trigger>
-				<Dialog.Content class="sm:max-w-106.25">
-					<Dialog.Header>
-						<Dialog.Title>Creer un personnel</Dialog.Title>
-						<Dialog.Description>
-							Ceci va vous permettre apres de creer de donner des roles a ces personnes
-						</Dialog.Description>
-					</Dialog.Header>
-					
-					<Dialog.Footer>
-						<Dialog.Close type="button" class={buttonVariants({ variant: 'outline' })}>
-							Cancel
-						</Dialog.Close>
-						<AlertDialog.Root>
-							<AlertDialog.Trigger class={buttonVariants({ variant: 'default' })}>
-								Ajouter
-							</AlertDialog.Trigger>
-							<AlertDialog.Content>
-								<AlertDialog.Header>
-									<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
-									<AlertDialog.Description>
-										This action cannot be undone. This will permanently delete your account and
-										remove your data from our servers.
-									</AlertDialog.Description>
-								</AlertDialog.Header>
-								<AlertDialog.Footer>
-									<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-									<AlertDialog.Action>Continue</AlertDialog.Action>
-								</AlertDialog.Footer>
-							</AlertDialog.Content>
-						</AlertDialog.Root>
-					</Dialog.Footer>
-				</Dialog.Content>
-			</form>
-		</Dialog.Root> -->
 	</div>
 </main>
--- src/routes/(app)/surveillant/+page.svelte
+++ src/routes/(app)/surveillant/+page.svelte
@@ -1,10 +1,44 @@
 <script lang="ts">
-
-
+	import { Input } from '$lib/components/ui/input';
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
 </script>
 
-<main class="flex-1 p-6 bg-sidebar text-sidebar-foreground">
-
-    
-
-</main>
+<div class="sticky top-0 z-50 flex justify-between m-4">
+	<Input type="search" placeholder="Rechercher un serverillant" class="max-w-md" />
+	<Dialog.Root>
+		<form>
+			<Dialog.Trigger type="button" class={buttonVariants({ variant: 'default' })}>
+				Nouveau
+			</Dialog.Trigger>
+			<Dialog.Content class="sm:max-w-[425px]">
+				<Dialog.Header>
+					<Dialog.Title>Ajouter un nouveau surveillant</Dialog.Title>
+					<Dialog.Description>Plus de surveillant ameliorera l'etabilssement.</Dialog.Description>
+				</Dialog.Header>
+				<div class="grid gap-4">
+					<div class="grid gap-3">
+						<Label for="cin">Name</Label>
+						<Input
+							id="cin"
+							type="number"
+							name="cin"
+							placeholder="entrer votre cin ici"
+							minlength={10}
+							maxlength={10}
+							required
+						/>
+					</div>
+				</div>
+				<Dialog.Footer>
+					<Dialog.Close type="button" class={buttonVariants({ variant: 'outline' })}>
+						Annuler
+					</Dialog.Close>
+					<Button type="submit">Confirmer</Button>
+				</Dialog.Footer>
+			</Dialog.Content>
+		</form>
+	</Dialog.Root>
+</div>
+<main class="m-4 min-h-full rounded-md bg-sidebar text-sidebar-foreground"></main>
