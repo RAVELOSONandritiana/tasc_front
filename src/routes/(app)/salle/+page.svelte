8a7954db0dba377c17318a6cfc1b6b90ae404a94
hashterx <hgbmichel@gmail.com>
2026-06-04 13:40:55 +0300

    formulaire for salle

--- src/routes/(app)/salle/+page.svelte
+++ src/routes/(app)/salle/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import * as InputGroup from '$lib/components/ui/input-group/index.js';
 	import SearchIcon from '@lucide/svelte/icons/search';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { buttonVariants } from '$lib/components/ui/button';
 </script>
 
 <main class="min-h-full rounded-md bg-sidebar p-4 text-sidebar-foreground">
@@ -11,6 +13,26 @@
 				<SearchIcon />
 			</InputGroup.Addon>
 		</InputGroup.Root>
-		
+		<Dialog.Root>
+			<form>
+				<Dialog.Trigger type="button" class={buttonVariants({ variant: 'default' })}>
+					Nouveau
+				</Dialog.Trigger>
+				<Dialog.Content class="sm:max-w-[425px]">
+					<Dialog.Header>
+						<Dialog.Title>Ajouter des salles pour enseigner</Dialog.Title>
+						<Dialog.Description
+							>Ces salles pourront apres etre utiliser pour affecter des cours</Dialog.Description
+						>
+					</Dialog.Header>
+					<Dialog.Footer>
+						<Dialog.Close type="button" class={buttonVariants({ variant: 'outline' })}>
+							Annuler
+						</Dialog.Close>
+						<Dialog.Close class={buttonVariants({ variant: 'default' })}>Confirmer</Dialog.Close>
+					</Dialog.Footer>
+				</Dialog.Content>
+			</form>
+		</Dialog.Root>
 	</div>
 </main>
