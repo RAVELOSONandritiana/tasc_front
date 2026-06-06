be24a8924b40a9efa5081df040773cd7deb7cdb8
hashterx <hgbmichel@gmail.com>
2026-06-06 11:38:52 +0300

    create search input

---
+++ src/lib/components/user/SearchInput.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import SearchIcon from '@lucide/svelte/icons/search';
+	import * as InputGroup from '$lib/components/ui/input-group/index.js';
+	let { value = $bindable(''), ...rest } = $props();
+</script>
+
+<InputGroup.Root class="max-w-md">
+	<InputGroup.Input {...rest} type="search" placeholder="Rechercher un eleve" bind:value />
+	<InputGroup.Addon>
+		<SearchIcon />
+	</InputGroup.Addon>
+</InputGroup.Root>
