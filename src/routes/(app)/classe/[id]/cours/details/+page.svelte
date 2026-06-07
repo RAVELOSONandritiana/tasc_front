2943b4c254d1fd32285383485d7b9b3c8ee3d985
hashterx <hgbmichel@gmail.com>
2026-06-07 20:36:51 +0300

    popup presence

--- src/routes/(app)/classe/[id]/cours/details/+page.svelte
+++ src/routes/(app)/classe/[id]/cours/details/+page.svelte
@@ -2,12 +2,14 @@
 	import * as Table from '$lib/components/ui/table/index.js';
 	import { Checkbox } from '$lib/components/ui/checkbox/index.js';
 	import { Label } from '$lib/components/ui/label';
+	import { Button } from '$lib/components/ui/button';
+	import Presence from '$lib/components/user/classe/Presence.svelte';
 	const student = [
 		{
 			id: 1,
 			name: 'RAVELOSON',
 			lastname: 'Andritiana Michel',
-			status: 0,
+			status: false,
 			age: 17,
 			absence: 19,
 			examen: true
@@ -16,7 +18,7 @@
 			id: 2,
 			name: 'RAVELOSON',
 			lastname: 'Andritiana Michel',
-			status: 0,
+			status: false,
 			age: 17,
 			absence: 19,
 			examen: true
@@ -25,7 +27,7 @@
 			id: 3,
 			name: 'RAVELOSON',
 			lastname: 'Andritiana Michel',
-			status: 0,
+			status: false,
 			age: 17,
 			absence: 19,
 			examen: true
@@ -34,7 +36,7 @@
 			id: 4,
 			name: 'RAVELOSON',
 			lastname: 'Andritiana Michel',
-			status: 0,
+			status: false,
 			age: 17,
 			absence: 19,
 			examen: true
@@ -43,7 +45,7 @@
 			id: 5,
 			name: 'RAVELOSON',
 			lastname: 'Andritiana Michel',
-			status: 0,
+			status: false,
 			age: 17,
 			absence: 19,
 			examen: true
@@ -52,22 +54,45 @@
 			id: 6,
 			name: 'RAVELOSON',
 			lastname: 'Andritiana Michel',
-			status: 0,
+			status: false,
 			age: 17,
 			absence: 19,
 			examen: false
 		}
 	];
+	let coursStart = $state(false);
+	let coursEnd = $state(false);
+
+	async function startCourse() {
+		if (coursStart == false && coursEnd == false) {
+			coursStart = true;
+		} else if (coursStart == true && coursEnd == false) {
+			coursStart = false;
+			coursEnd = true;
+		}
+	}
+
+	let open = $state(false);
 </script>
 
 <main class="p-4">
-	<div class="space-y-4">
-		<Label>Prof Titulaire: RAKOTO BE</Label>
-		<Label>Coefficient: 6</Label>
+	<div class="flex items-start justify-between">
+		<div class="space-y-4">
+			<Label>Prof Titulaire: RAKOTO BE</Label>
+			<Label>Coefficient: 6</Label>
+		</div>
+		<div class="flex items-center justify-center space-x-4">
+			<Button onclick={() => startCourse()} disabled={coursEnd}
+				>{coursStart ? 'Terminer Cours' : coursEnd ? 'Cours Termine' : 'Commencer Cours'}</Button
+			>
+			{#if coursStart == true}
+				<Presence bind:open />
+			{/if}
+		</div>
 	</div>
 
 	<div class="pt-4">
-        <Table.Root>
+		<Table.Root>
 			<Table.Header>
 				<Table.Row>
 					<Table.Head>examen</Table.Head>
@@ -82,7 +107,7 @@
 				{#each student as s (s.id)}
 					<Table.Row>
 						<Table.Cell>
-							<Checkbox />
+							<Checkbox checked={s.status} />
 						</Table.Cell>
 						<Table.Cell>{s.name}</Table.Cell>
 						<Table.Cell>{s.lastname}</Table.Cell>
