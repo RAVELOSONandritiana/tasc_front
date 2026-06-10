63697d62bc370c7924fdf00f65ce70993b8ad200
hashterx <hgbmichel@gmail.com>
2026-06-10 18:35:35 +0300

    add personnel button

--- src/routes/(app)/personne/+page.svelte
+++ src/routes/(app)/personne/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import PersonnelCard from '$lib/components/user/profil/PersonnelCard.svelte';
 	import SearchInput from '$lib/components/user/SearchInput.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import type { PageProps } from './$types';
+	import { goto } from '$app/navigation';
 
 	const { data }: PageProps = $props();
 	const { personnes } = data;
@@ -15,11 +17,16 @@
 				.includes(searchText.toLowerCase())
 		)
 	);
+
+	function goNew() {
+		goto('/personne/new');
+	}
 </script>
 
 <main class="min-h-full bg-sidebar text-sidebar-foreground">
 	<div class="flex justify-between p-4 top-16 sticky z-50 bg-sidebar">
 		<SearchInput bind:value={searchText} placeholder="Rechercher une personne" />
+		<Button class="h-9 rounded-lg px-5 text-sm font-medium" onclick={goNew}> Nouveau </Button>
 	</div>
 
 	<div class="p-4">
